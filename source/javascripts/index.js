b95e931b6de6a90be72679c5b2bd194ba86326f8
Hirofumi Jinno <hirofumi.jinno@gmail.com>
2021-12-28 16:52:15 -0500

    boostrap js added

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -2,6 +2,7 @@ import '../stylesheets/index.sass';
 import initShowcase from './initShowcase';
 import initOffcanvasMenu from './initOffcanvasMenu';
 import initSlider from './initSlider';
+import 'bootstrap';
 
 initShowcase();
 initOffcanvasMenu();
